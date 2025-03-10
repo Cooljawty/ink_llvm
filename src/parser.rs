c1cd2378ce1fa5da74e025094b9a4f9899ef25e1
Jacari Harper <jacariguyman2@gmail.com>
2025-03-10 13:23:24 -0700

    Fixed parseing knot body till eof

--- src/parser.rs
+++ src/parser.rs
@@ -3,8 +3,8 @@ use nom::IResult;
 #[allow(unused_imports)]
 use nom::{
     Parser,
-    multi::{many0, many_till},
-    bytes::{tag, is_a, take_until,},
+    multi::{many0, many_till, fold_many0},
+    bytes::{tag, is_a, take_until,take_till},
     character::{
         anychar,
         complete::{
@@ -13,7 +13,7 @@ use nom::{
             line_ending, not_line_ending,
         },
     },
-    combinator::{value, opt, eof, not, peek, recognize,success,},
+    combinator::{value, opt, eof, not, peek, recognize,success,all_consuming,},
     branch::{alt,},
 };
 
@@ -40,21 +40,41 @@ where
     for<'parser> &'parser str: nom::FindToken<<I as nom::Input>::Item> 
 {   
     println!("Parsing knot");
-    let (rem, _) = knot_signature.parse(input)?;
+    let (rem, _signature) = knot_signature.parse(input)?;
     let (rem, _body) = knot_body.parse(rem)?;
+
+    //let (rem, _body) = knot_body.parse(rem)?;
     println!("End of knot");
     Ok((rem, ast::Subprogram::Knot))
     //value(ast::Subprogram::Knot, (knot_signature, knot_body)).parse(input)
 }
 
-fn knot_body<I>(input: I) -> IResult<I, ast::Subprogram> 
+fn knot_body<I, T>(input: I) -> IResult<I, ast::Subprogram> 
 where
-	for<'parser> I: nom::Input + nom::Offset + nom::Compare<&'parser str> + nom::FindSubstring<&'parser str>,
+	for<'parser> I: nom::Input + nom::Offset + nom::Compare<&'parser str> + nom::FindSubstring<&'parser str> + nom::FindSubstring<T>,
     <I as nom::Input>::Item: nom::AsChar,
     for<'parser> &'parser str: nom::FindToken<<I as nom::Input>::Item> 
 { 
     println!("Parsing body");
-    value(ast::Subprogram::Knot, many_till(recognize((anychar, anychar)), peek(alt((tag("=="), eof))))).parse(input) 
+    //value(ast::Subprogram::Knot, many_till( recognize(      (anychar, anychar)  ),  peek(alt(    (tag("=="), eof)                ))  )).parse(input) 
+
+    //println!("\n----\n{}\n<<<<\n", chunk.iter().map(nom::Input::iter_elements).flatten().map(nom::AsChar::as_char).collect::<String>());
+    //println!("End body");
+    //Ok((rem, ast::Subprogram::Knot))
+    let (rem, body) = match take_until("==").parse(input.clone()) {
+        Ok((rem, body)) => {
+            peek(recognize(knot_signature)).parse(rem)?
+        },
+        nom::IResult::Err(nom::Err::Incomplete(_)) => {
+            println!("\teof!");
+            input.take_split(input.input_len()-1)
+        },
+        err => err?
+    };
+    println!("\n>>>>\n{}\n>>>>\n", body.iter_elements().map(nom::AsChar::as_char).collect::<String>());
+    println!("\n<<<<\n{}\n<<<<\n", rem.iter_elements().map(nom::AsChar::as_char).collect::<String>());
+    
+    Ok((rem, ast::Subprogram::Knot))
 }
 
 fn knot_signature<I>(input: I) -> IResult<I, ast::Subprogram> 
@@ -64,10 +84,13 @@ where
     for<'parser> &'parser str: nom::FindToken<<I as nom::Input>::Item> 
 { 
     println!("Parsing signature");
-    let (rem, _) = (tag("=="), opt(is_a("=")), space0).parse(input)?;
+    let (rem, _) = (space0, tag("=="), opt(is_a("=")), space0).parse(input)?;
     let (rem, name) = identifier.parse(rem)?;
     println!("\tname: {}", name);
-    value(ast::Subprogram::Knot, (space0, opt(is_a("=")), line_ending)).parse(rem)
+    let (rem, _) = (space0, opt(is_a("=")), line_ending).parse(rem)?;
+    println!("End signature");
+    Ok((rem, ast::Subprogram::Knot))
+    //value(ast::Subprogram::Knot, (space0, opt(is_a("=")), line_ending)).parse(rem)
 }
 
 fn identifier<I>(input: I) -> IResult<I, String> 
